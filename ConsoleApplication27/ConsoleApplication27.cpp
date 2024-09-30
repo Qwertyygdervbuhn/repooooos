651b55bb6f363e00fa8981a86eb51b3a511df4de
Alexander Dm. <dabl21291@gmail.com>
2024-09-30 23:12:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication27/ConsoleApplication27.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+class Class {
+private:
+    int numerator;  
+    int denominator; 
+
+public:
+  
+    Class() {
+        numerator = 0;
+        denominator = 1;
+    }
+   
+    Class(int num, int denom) {
+        numerator = num;
+
+        if (denom == 0) {
+            cout << "Error! denominator = 0!\n";
+            denominator = 1; 
+        }
+        else {
+            denominator = denom;
+        }
+    }
+    void display() {
+        cout << numerator << "/" << denominator << endl;
+    }
+};
+
+int main() {
+   
+    Class obj1;
+    obj1.display();
+ 
+    Class obj2(3, 4);
+    obj2.display();
+
+    Class obj3(5, 0);
+    obj3.display();
+
+}
+
